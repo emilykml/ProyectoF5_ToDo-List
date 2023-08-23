ebfa537b42a5ebd50e005553b1fc19ee462240c7
Emily <emily.artfree@gmail.com>
2023-08-23 20:04:25 +0200

    nuevo archivo

---
+++ functions.js
@@ -0,0 +1,24 @@
+
+//declaro mis variables//
+
+const fecha = document.querySelector("#fecha")
+const lista = document.querySelector("#lista")
+const input = document.querySelector("#input")
+const enterButton = document.querySelector("#enter-button")
+
+//------FUNCIONES---------//
+
+//AGREGAR TAREA//
+``
+
+
+
+//ELIMINAR TAREA//
+
+
+
+//EDITAR TAREA//
+
+
+
+
